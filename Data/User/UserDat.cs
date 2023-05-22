ae970e121a39f4e9c5e6acdf9f6fa265da24daf4
Diego Alzate <diegoalzate.0207@gmail.com>
2023-05-22 17:49:33 -0500

    Subiendo cambios a la capa DATA

---
+++ Data/User/UserDat.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace Data.User
+{
+    public class UserDat
+    {
+        Persistence objPer = new Persistence();
+        public DataSet showUser()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.OpenConnection();
+            objSelectCmd.CommandText = "procSelectUser";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.CloseConnection();
+            return objData;
+        }
+
+        public bool saveUser(string _mail_user, int _password_user, string _type_user)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.OpenConnection();
+            objSelectCmd.CommandText = "procInsertUser"; //Nombre del procedimiento almacenado.
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("mail_user", MySqlDbType.VarString).Value = _mail_user;
+            objSelectCmd.Parameters.Add("password_user", MySqlDbType.Int32).Value = _password_user;
+            objSelectCmd.Parameters.Add("type_user", MySqlDbType.VarString).Value = _type_user;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            objPer.CloseConnection();
+            return executed;
+        }
+
+        public bool updateUser(int _id_user, string _mail_user, int _password_user, string _type_user)
+        {
+            bool execute = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.OpenConnection();
+            objSelectCmd.CommandText = "procUpdateUser";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("id_user", MySqlDbType.Int32).Value = _id_user;
+            objSelectCmd.Parameters.Add("mail_user", MySqlDbType.VarString).Value = _mail_user;
+            objSelectCmd.Parameters.Add("password_user", MySqlDbType.Int32).Value = _password_user;
+            objSelectCmd.Parameters.Add("type_user", MySqlDbType.VarString).Value = _type_user;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    execute = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            objPer.CloseConnection();
+            return execute;
+        }
+
+        public bool deleteUser(int _id_user)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.OpenConnection();
+            objSelectCmd.CommandText = "procDeleteUser";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_id_country", MySqlDbType.Int32).Value = _id_user;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            objPer.CloseConnection();
+            return executed;
+        }
+
+    }
+}
---
+++ Data/Vehicle/VehicleDat.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Vehicle
+{
+    public class VehicleDat
+    {
+        Persistence objPer = new Persistence();
+        public DataSet showVehicle()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.OpenConnection();
+            objSelectCmd.CommandText = "procSelectVehicle";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.CloseConnection();
+            return objData;
+        }
+
+        public bool saveVehicle(string _plate_vehicle, int _model_vehicle, string _type_vehicle, string _capacityLoading_vehicle, int _id_driver)
+        {
+            bool executed = false;
+            int row;
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.OpenConnection();
+            objSelectCmd.CommandText = "procInsertVehicle";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("plate_vehicle", MySqlDbType.VarChar).Value = _plate_vehicle;
+            objSelectCmd.Parameters.Add("model_vehicle", MySqlDbType.Int32).Value = _model_vehicle;
+            objSelectCmd.Parameters.Add("type_vehicle", MySqlDbType.VarChar).Value = _type_vehicle;
+            objSelectCmd.Parameters.Add("capacityLoading_vehicle", MySqlDbType.VarChar).Value = _capacityLoading_vehicle;
+            objSelectCmd.Parameters.Add("id_driver", MySqlDbType.Int32).Value = _id_driver;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.ToString());
+            }
+            objPer.CloseConnection();
+            return executed;
+        }
+
+        public bool updateVehicle(int _id_vehicle, string _plate_vehicle, int _model_vehicle, string _type_vehicle, string _capacityLoading_vehicle, int _id_driver)
+        {
+            bool executed = false;
+            int row;
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.OpenConnection();
+            objSelectCmd.CommandText = "procUpdateVehicle";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("id_vehicle", MySqlDbType.Int32).Value = _id_vehicle;
+            objSelectCmd.Parameters.Add("plate_vehicle", MySqlDbType.VarChar).Value = _plate_vehicle;
+            objSelectCmd.Parameters.Add("model_vehicle", MySqlDbType.Int32).Value = _model_vehicle;
+            objSelectCmd.Parameters.Add("type_vehicle", MySqlDbType.VarChar).Value = _type_vehicle;
+            objSelectCmd.Parameters.Add("capacityLoading_vehicle", MySqlDbType.VarChar).Value = _capacityLoading_vehicle;
+            objSelectCmd.Parameters.Add("id_driver", MySqlDbType.Int32).Value = _id_driver;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.ToString());
+            }
+            objPer.CloseConnection();
+            return executed;
+        }
+
+        public bool deleteVehicle(int _id_vehicle)
+        {
+            bool executed = false;
+            int row;
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.OpenConnection();
+            objSelectCmd.CommandText = "spDeleteVehicle";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("id_vehicle", MySqlDbType.Int32).Value = _id_vehicle;
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.ToString());
+            }
+            objPer.CloseConnection();
+            return executed;
+        }
+    }
+}
